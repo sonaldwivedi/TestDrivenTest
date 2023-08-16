89df015a73cdf7a20a968ded3ee84619060e3617
Bhumish Gajjar <sonaldwivedi@hotmail.com>
2023-08-16 14:32:01 +0530

    updated

--- src/test/java/com/testcases/StringSwap.java
+++ src/test/java/com/testcases/StringSwap.java
@@ -4,39 +4,33 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.base.StringSwapHelper;
+
 public class StringSwap {
 	
+	StringSwapHelper help=new StringSwapHelper();
+	
 	@Test
-	public void stringSwap2charsOnly() {
-		StringSwapHelper help=new StringSwapHelper();
-		Assert.assertEquals("BA", help.swaplasttwochars("AB"));
-		
+	public void stringSwap2charsOnly() {		
+		Assert.assertEquals("BA", help.swaplasttwochars("AB"));		
 	} 
 	
 	@Test
-	public void stringSwap4chars() {
-		StringSwapHelper help=new StringSwapHelper();
-		Assert.assertEquals("ABDC", help.swaplasttwochars("ABCD"));
-		
+	public void stringSwap4chars() {		
+		Assert.assertEquals("ABDC", help.swaplasttwochars("ABCD"));		
 	} 
 	
 	@Test
 	public void stringSwap() {
-		StringSwapHelper help=new StringSwapHelper();
-		Assert.assertEquals("SELENIMU", help.swaplasttwochars("SELENIUM"));
-		
+		Assert.assertEquals("SELENIMU", help.swaplasttwochars("SELENIUM"));		
 	} 
 	
 	@Test
 	public void stringSwap1CharOnly() {
-		StringSwapHelper help=new StringSwapHelper();
-		Assert.assertEquals("A", help.swaplasttwochars("A"));
-		
+		Assert.assertEquals("A", help.swaplasttwochars("A"));		
 	} 
 	
 	@Test
 	public void stringSwap0Char() {
-		StringSwapHelper help=new StringSwapHelper();
 		Assert.assertEquals("", help.swaplasttwochars(""));
 		
 	} 
